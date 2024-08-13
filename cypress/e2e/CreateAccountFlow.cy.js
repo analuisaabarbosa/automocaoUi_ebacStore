539b885e25f50ea875fea253f03a52ca2fc8bc8c
analuisa <analuisaaugustob@gmail.com>
2024-08-13 16:15:36 -0300

    :test_tube:test: Escrevendo testes.

---
+++ cypress/e2e/CreateAccountFlow.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+const { accountSignUpPage } = require("../support/pages/accountSignUp.page");
+const { homePage } = require("../support/pages/home.page");
+
+describe('Account Creation Flow', () => {
+    
+    beforeEach(() => {
+        cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' }),
+        cy.visit('/')
+    });
+
+    it('Should allow a user to successfully create an account', () => {
+        homePage.openMenu('Account')
+        accountSignUpPage.signUp()
+    });
+
+});
