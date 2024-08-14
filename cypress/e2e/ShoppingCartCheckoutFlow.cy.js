ffbb50f2fd8804c2e8198e417a6d2788f3a7f80b
analuisa <analuisaaugustob@gmail.com>
2024-08-14 15:32:14 -0300

    :ok_hand:style: Alterações de revisão de código.

--- cypress/e2e/ShoppingCartCheckoutFlow.cy.js
+++ cypress/e2e/ShoppingCartCheckoutFlow.cy.js
@@ -1,1 +1,21 @@
 /// <reference types="cypress" />
+
+const { email, password } = require("../fixtures/data.json");
+
+describe('Shopping cart flow', () => {
+    
+    beforeEach(() => {
+        cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' }),
+        cy.visit('/')
+    });
+
+    it('Must complete the checkout successfully', () => {
+        cy.login(email, password)
+        cy.purchaseFlow()
+    });
+
+    afterEach(() => {
+        cy.emptyingShoppingCart()
+    });
+
+});
