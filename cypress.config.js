6fa40781cbf8284565efa4869cd3fea1a06eb4bc
analuisa <analuisaaugustob@gmail.com>
2024-09-23 09:28:32 -0300

    Adicionando nova variável de ambiente do projeto.

--- cypress.config.js
+++ cypress.config.js
@@ -10,8 +10,9 @@ module.exports = defineConfig({
       
     },
     env: {
-      email: process.env.EMAIL,
-      password: process.env.PASSWORD
+      EMAIL: process.env.EMAIL,
+      PASSWORD: process.env.PASSWORD,
+      USER_ID: process.env.CYPRESS_USER_ID
     }
   },
 });
