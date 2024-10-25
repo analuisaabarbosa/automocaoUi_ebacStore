0fab286b64670a4e8be7b2b9787f4dbbb94c6488
Analu <analuisaaugustob@gmail.com>
2024-10-25 18:42:04 -0300

    Configurações do ambiente de desenvolvimento

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,10 @@
-const { defineConfig } = require('cypress');
-const dotenv = require('dotenv');
-
-dotenv.config();
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    baseUrl: "http://lojaebac.ebaconline.art.br/",
     setupNodeEvents(on, config) {
       
-    },
-    env: {
-      email: process.env.EMAIL,
-      password: process.env.PASSWORD
     }
-  },
+  }
 });
